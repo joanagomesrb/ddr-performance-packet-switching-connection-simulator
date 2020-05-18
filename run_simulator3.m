d02f9fb5c258e97e2d50e3d74b94cc781ab497f7
Miguel Lopes <m.lopes@ua.pt>
2020-05-18 13:19:18 +0100

    added sim3

--- run_simulator3.m
+++ run_simulator3.m
@@ -1,7 +1,7 @@
 % INPUT PARAMETERS:
 lambda = 200; %  lambda - packet rate (packets/sec)
 C = 2;        %  C      - link bandwidth (Mbps)
-f = 100000;     %  f      - queue size (Bytes)
+f = 10000;     %  f      - queue size (Bytes)
 P = 100000;    %  P      - number of packets (stopping criterium)
 nvoip = 15;
 % run simulator parameters
@@ -19,15 +19,15 @@ result_MPvoip = zeros(1, n_times);
 
 % actual run simulator n times
 for i = 1:n_times
-    [PL , APD , MPD , TT, PLvoip, APDvoip, MPDvoip] = simulator2(lambda,C,f,P,nvoip);
+    [PL , APD , MPD , TT, PLvoip, APDvoip, MPDvoip] = simulator3(lambda,C,f,P,nvoip);
     result_PL(i) = PL;
     result_APD(i) = APD;
     result_MP(i) = MPD;
     result_TT(i) = TT;
     
-    result_PLvoip(i) = PLvoip;
-    result_APDvoip(i) = APDvoip;
-    result_MPvoip(i) = MPDvoip;
+    result_PLvoip = PLvoip;
+    result_APDvoip = APDvoip;
+    result_MPvoip = MPDvoip;
 end
 
 % 90% confidence interval PL
